f457e14bf24d0b83c274d620238f3088cf5dfc3e
Sunny Oswal <sunny_oswal@outlook.com>
2019-06-13 16:49:49 +0800

    Create Update-ARMTemplateParametersFile.ps1

---
+++ Powershell/Update-ARMTemplateParametersFile.ps1
@@ -0,0 +1,43 @@
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $parametersFilePath
+)
+
+try
+{
+    #Read parameters.json file
+    $paramsFile = Get-Content -Path $parametersFilePath | ConvertFrom-Json 
+    
+    #Initialize Updated Parameters object
+    $updatedParam = New-Object -TypeName PSObject
+    $updatedParam | Add-Member -MemberType NoteProperty -Name '$schema' -Value $paramsFile.'$schema'
+    $updatedParam | Add-Member -MemberType NoteProperty -Name "contentVersion" -Value $paramsFile.contentVersion
+    
+    $paramHashTable = @{}
+    foreach($param in $paramsFile.parameters.psobject.Properties)
+    {
+        $paramValueHashTable = @{}
+        if($param.Value.value -eq "")
+        {
+            #Get values from variables
+            $paramKey = $param.Name
+            $secretValue = (get-item env:$paramKey).Value
+
+            $paramValueHashTable["value"] = $secretValue
+            $paramHashTable[$param.Name] = $paramValueHashTable
+        }
+        else
+        {
+            $paramValueHashTable["value"] = $param.Value.value
+            $paramHashTable[$param.Name] = $paramValueHashTable
+        }
+    }
+    
+    $updatedParam | Add-Member -MemberType NoteProperty -Name "parameters" -Value $paramHashTable
+    $updatedParam | ConvertTo-Json | Out-File $parametersFilePath -Encoding utf8 -Force
+}
+catch
+{
+    throw $Error[0]
+}
