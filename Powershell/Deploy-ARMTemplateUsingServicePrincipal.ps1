0d7cc29e6778d49b17b8dc73b8ebbc83e7b0e756
Sunny Oswal <sunny_oswal@outlook.com>
2019-06-14 10:32:26 +0800

    Update Deploy-ARMTemplateUsingServicePrincipal.ps1

--- Powershell/Deploy-ARMTemplateUsingServicePrincipal.ps1
+++ Powershell/Deploy-ARMTemplateUsingServicePrincipal.ps1
@@ -5,6 +5,7 @@ Param
 [Parameter(Mandatory=$True)] $tenantId,
 [Parameter(Mandatory=$True)] $subscriptionId,
 [Parameter(Mandatory=$True)] $resourceGroup,
+[Parameter(Mandatory=$True)] $region,
 [Parameter(Mandatory=$True)] $templateFilePath,
 [Parameter(Mandatory=$True)] $paramsFilePath
 )
@@ -17,5 +18,6 @@ Write-Host "---------------------------------------"
 $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
 
-Connect-AzAccount -Credential $credential -Tenant $tenantId -SubscriptionId $subscriptionId -ServicePrincipal 
+Connect-AzAccount -Credential $credential -Tenant $tenantId -SubscriptionId $subscriptionId -ServicePrincipal
+New-AzResourceGroup -Name $resourceGroup -Location $region -Force
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $templateFilePath -TemplateParameterFile $paramsFilePath
